f37f3bbc059aa92513a15267d883e05ce01abbcd
cassymarie <cassy2205@gmail.com>
2021-02-05 10:58:23 -0700

    commit tree f811c9d1e88559ee0cef55f06fc658423f09a011

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(driverArray, searchText){
+    if (driverArray.length === 0){return}
+   return driverArray.filter(obj => {
+       debugger
+        obj.name.toLowerCase() === searchText.toLowerCase();
+    })
+    } 
+
+function fuzzyMatch(driverArray, searchText){
+    return driverArray.filter(obj => {
+        obj.name.charAt(searchText.length) === searchText
+    })
+    }
+
+function matchName(driverArray, searchText){
+    return driverArray.filter(obj => {obj['name'] === searchText})
+    }
